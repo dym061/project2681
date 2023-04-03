f73c4a21bcf3d4142bbe5bda6adc16fd5486b17e
dym061 <dominik.vonfrancois@hotmail.com>
2023-04-03 10:27:35 +0200

    project restrictions and limitations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import Prevent from "./prevent";
 config.autoAddCss = false;
 
 import Head from 'next/head';
@@ -12,7 +13,7 @@ export default function App({ Component, pageProps }) {
 		    <title>Seoul Food - Authentic Korean Takeaway in Namibia</title> 
 		    <meta name="robots" content="noindex"/>
 		  </Head>
-
+		  <Prevent />
 		  <Component {...pageProps} />
 		</>
 		);
